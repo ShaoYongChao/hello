f9de9ced42f2f2afb96ef09bfa929394a16a5280
star <yongchao.shao@gmail.com>
2023-06-07 17:04:52 +0800

    add mind

---
+++ mind/simple-mind-map/full.js
@@ -0,0 +1,28 @@
+import MindMap from './index'
+import MiniMap from './src/MiniMap.js'
+import Watermark from './src/Watermark.js'
+import KeyboardNavigation from './src/KeyboardNavigation.js'
+import Export from './src/Export.js'
+import Drag from './src/Drag.js'
+import Select from './src/Select.js'
+import AssociativeLine from './src/AssociativeLine'
+import RichText from './src/RichText'
+import xmind from './src/parse/xmind.js'
+import markdown from './src/parse/markdown.js'
+import icons from './src/svg/icons.js'
+
+MindMap.xmind = xmind
+MindMap.markdown = markdown
+MindMap.iconList = icons.nodeIconList
+
+MindMap
+  .usePlugin(MiniMap)
+  .usePlugin(Watermark)
+  .usePlugin(Drag)
+  .usePlugin(KeyboardNavigation)
+  .usePlugin(Export)
+  .usePlugin(Select)
+  .usePlugin(AssociativeLine)
+  .usePlugin(RichText)
+
+export default MindMap
